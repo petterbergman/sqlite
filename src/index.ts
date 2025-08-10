f5d8702b26f336189d0d23a62f2b3fad80ae741f
petterbergman <petterbergman@gmail.com>
2025-08-10 19:16:55 +0200

    prettier fix
    (cherry picked from commit 8047e3f77ab19dea104b95db209851f6183396fb)

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import { registerPlugin } from '@capacitor/core';
 import type { CapacitorSQLitePlugin } from './definitions';
 
 const CapacitorSQLite = registerPlugin<CapacitorSQLitePlugin>('CapacitorSQLite', {
-  web: () => import('./web').then((m) => new m.CapacitorSQLiteWeb())
+  web: () => import('./web').then((m) => new m.CapacitorSQLiteWeb()),
 });
 
 export { CapacitorSQLite };
